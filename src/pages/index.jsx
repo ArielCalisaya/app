2791a33d1b1b6c40c1d5bf30a997bd53f769213a
Ariel Calisaya <calisayaariel62@gmail.com>
2022-08-23 14:28:45 -0400

    test socket io

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,9 +13,10 @@ import {
 import PropTypes from 'prop-types';
 import { Formik, Form, Field } from 'formik';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import getT from 'next-translate/getT';
 import Image from 'next/image';
+import io from 'socket.io-client';
 import Heading from '../common/components/Heading';
 import Text from '../common/components/Text';
 import Icon from '../common/components/Icon';
@@ -54,12 +55,13 @@ export default function Home() {
   const socials = t('social:content', {}, { returnObjects: true });
   const socialsFiltered = socials.filter((social) => social.available.includes('home'));
 
-  console.log('router.asPath', router.asPath);
-  // useEffect(() => {
-  //   // disconnect from server when leaving the page
-  //   const socket = io();
-  //   socket.emit('user-disconnection', { userId: profile.id });
-  // }, [router]);
+  useEffect(() => {
+    // disconnect from server when leaving the page
+    const socket = io();
+    socket.on('now', (data) => {
+      console.log('data received from server', data);
+    });
+  }, []);
 
   const BubblesSvg = () => (
     <svg
